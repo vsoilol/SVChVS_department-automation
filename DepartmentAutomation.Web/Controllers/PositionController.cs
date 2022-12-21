145d99268b6b6613e999e3a0f35c816fa158b16f
Vladislav <vsoilol1701@gmail.com>
2022-12-21 19:45:56 +0300

    feat: add PositionController

---
+++ DepartmentAutomation.Web/Controllers/PositionController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DepartmentAutomation.Application.Common.Attributes;
+using DepartmentAutomation.Application.Contracts.Responses;
+using DepartmentAutomation.Application.Features.Positions.Queries.GetAllPositions;
+using DepartmentAutomation.Domain.Enums;
+using DepartmentAutomation.Web.Contracts;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DepartmentAutomation.Web.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [AuthorizeRoles(Role.Admin)]
+    public class PositionController : ApiControllerBase
+    {
+        [HttpGet(ApiRoutes.Position.GetAll)]
+        public async Task<ActionResult<List<PositionDto>>> GetAll()
+        {
+            return await Mediator.Send(new GetAllPositionsQuery());
+        }
+    }
+}
