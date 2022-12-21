d092d84202264307865a104db8b3769dee1de2ad
Nastya Kisliuk <kisliuk.nastya@gmail.com>
2022-12-21 19:34:10 +0300

    feat: add seagger examples

---
+++ DepartmentAutomation.Web/SwaggerExamples/Requests/UserRegistrationRequestExample.cs
@@ -0,0 +1,18 @@
+﻿using DepartmentAutomation.Application.Contracts.Requests;
+using DepartmentAutomation.Application.Contracts.Requests.Identity;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace DepartmentAutomation.Web.SwaggerExamples.Requests
+{
+    public class UserRegistrationRequestExample : IExamplesProvider<UserRegistrationRequest>
+    {
+        public UserRegistrationRequest GetExamples()
+        {
+            return new UserRegistrationRequest
+            {
+                Email = "email@mail.com",
+                Password = "S@1tring",
+            };
+        }
+    }
+}
