af1bec45ebcbf7f4d4f54cb25b58b09170729f17
Nastya Kisliuk <kisliuk.nastya@gmail.com>
2022-12-21 19:43:20 +0300

    feat: add CompetenceFormationLevelController

---
+++ DepartmentAutomation.Web/Controllers/CompetenceFormationLevelController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DepartmentAutomation.Application.Common.Attributes;
+using DepartmentAutomation.Application.Contracts.Responses;
+using DepartmentAutomation.Application.Features.CompetenceFormationLevels.Commands.UpdateCompetenceFormationLevel;
+using DepartmentAutomation.Application.Features.CompetenceFormationLevels.Queries.
+    GetCompetenceFormationLevelByCompetence;
+using DepartmentAutomation.Domain.Enums;
+using DepartmentAutomation.Web.Contracts;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DepartmentAutomation.Web.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [AuthorizeRoles(Role.Teacher)]
+    public class CompetenceFormationLevelController : ApiControllerBase
+    {
+        [HttpGet(ApiRoutes.CompetenceFormationLevel.GetCompetenceFormationLevelByCompetenceId)]
+        public async Task<ActionResult<List<CompetenceFormationLevelDto>>>
+            GetCompetenceFormationLevelByCompetenceIdAsync(
+                [FromQuery] GetCompetenceFormationLevelByCompetenceQuery query)
+        {
+            return await Mediator.Send(query);
+        }
+
+        [HttpPut(ApiRoutes.CompetenceFormationLevel.Base)]
+        public async Task<ActionResult> UpdateCompetenceFormationLevelAsync(
+            [FromBody] UpdateCompetenceFormationLevelCommand command)
+        {
+            await Mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
