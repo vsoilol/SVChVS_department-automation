633007d354ca995a71c277f28d63ed2867f37e71
Nastya Kisliuk <kisliuk.nastya@gmail.com>
2022-12-21 19:44:34 +0300

    feat: add IndicatorController

---
+++ DepartmentAutomation.Web/Controllers/IndicatorController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DepartmentAutomation.Application.Common.Attributes;
+using DepartmentAutomation.Application.Contracts.Responses;
+using DepartmentAutomation.Application.Features.Indicators.Queries.GetIndicatorWithLevelsByProgramId;
+using DepartmentAutomation.Domain.Enums;
+using DepartmentAutomation.Web.Contracts;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DepartmentAutomation.Web.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [AuthorizeRoles(Role.Teacher, Role.Admin)]
+    public class IndicatorController : ApiControllerBase
+    {
+        [HttpGet(ApiRoutes.Indicator.GetIndicatorWithLevelsByProgramId)]
+        public async Task<ActionResult<List<IndicatorWithLevelsDto>>> GetIndicatorWithLevelsByProgramIdAsync(
+            [FromRoute] int educationalProgramId)
+        {
+            return await Mediator.Send(new GetIndicatorWithLevelsByProgramIdQuery
+                { EducationalProgramId = educationalProgramId });
+        }
+    }
+}
