6c38808595dd32b145b39fd65ebaaa289ce666cc
Nastya Kisliuk <kisliuk.nastya@gmail.com>
2022-12-21 19:35:48 +0300

    feat: add configs

---
+++ DepartmentAutomation.Web/Config/JwtTokenConfig.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using DepartmentAutomation.Application.Common.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DepartmentAutomation.Web.Config
+{
+    public static class JwtTokenConfig
+    {
+        public static void SetupJwtTokens(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = new JwtSettings();
+            configuration.Bind(nameof(JwtSettings), jwtSettings);
+            services.AddSingleton(jwtSettings);
+
+            // configure jwt authentication
+            var key = Encoding.UTF8.GetBytes(jwtSettings.Secret);
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                RequireExpirationTime = false,
+                ValidateLifetime = false,
+            };
+
+            services.AddSingleton(tokenValidationParameters);
+
+            services.AddAuthentication(x =>
+                {
+                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(x =>
+                {
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = tokenValidationParameters;
+                });
+
+            services.AddAuthorization();
+        }
+    }
+}
---
+++ DepartmentAutomation.Web/Config/SwaggerConfig.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace DepartmentAutomation.Web.Config
+{
+    public static class SwaggerConfig
+    {
+        public static void SetupSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(_ =>
+            {
+                _.SwaggerDoc("v1", new OpenApiInfo { Title = "DepartmentAutomation", Version = "v1" });
+
+                _.ExampleFilters();
+
+                _.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description =
+                        "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                });
+
+                _.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Id = "Bearer", // The name of the previously defined security scheme.
+                                Type = ReferenceType.SecurityScheme,
+                            },
+                        },
+                        new List<string>()
+                    },
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                _.IncludeXmlComments(xmlPath);
+            });
+
+            services.AddSwaggerExamplesFromAssemblyOf<Startup>();
+        }
+    }
+}
