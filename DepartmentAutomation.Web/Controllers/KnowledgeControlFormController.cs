5d65606839d4e7b99b971fb8ac3e97463b7b7c58
Vladislav <vsoilol1701@gmail.com>
2022-12-21 19:45:13 +0300

    feat: add KnowledgeControlFormController

---
+++ DepartmentAutomation.Web/Controllers/KnowledgeControlFormController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DepartmentAutomation.Application.Common.Attributes;
+using DepartmentAutomation.Application.Contracts.Responses;
+using DepartmentAutomation.Application.Features.KnowledgeControlForms.Queries.GetAllKnowledgeControlForm;
+using DepartmentAutomation.Application.Features.KnowledgeControlForms.Queries.GetKnowledgeControlFormsByWeekId;
+using DepartmentAutomation.Domain.Enums;
+using DepartmentAutomation.Web.Contracts;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DepartmentAutomation.Web.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [AuthorizeRoles(Role.Teacher)]
+    public class KnowledgeControlFormController : ApiControllerBase
+    {
+        [HttpGet(ApiRoutes.KnowledgeControlForm.GetAllByWeekId)]
+        public async Task<ActionResult<List<KnowledgeAssessmentDto>>> GetAllByWeekIdAsync(
+            [FromRoute] int weekId)
+        {
+            return await Mediator.Send(new GetKnowledgeControlFormsByWeekIdQuery { WeekId = weekId });
+        }
+
+        [HttpGet(ApiRoutes.KnowledgeControlForm.Base)]
+        public async Task<ActionResult<List<KnowledgeControlFormDto>>> GetAllAsync()
+        {
+            return await Mediator.Send(new GetAllKnowledgeControlFormQuery());
+        }
+    }
+}
