2569fe43d81cbad06bcdd85e7099fd575db77b42
Vladislav <vsoilol1701@gmail.com>
2022-12-21 19:46:06 +0300

    feat: add ReviewerController

---
+++ DepartmentAutomation.Web/Controllers/ReviewerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using DepartmentAutomation.Application.Common.Attributes;
+using DepartmentAutomation.Application.Contracts.Responses;
+using DepartmentAutomation.Application.Features.Reviewers.Commands.UpdateReviewer;
+using DepartmentAutomation.Application.Features.Reviewers.Queries.GetReviewerByProgramId;
+using DepartmentAutomation.Domain.Enums;
+using DepartmentAutomation.Web.Contracts;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DepartmentAutomation.Web.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [AuthorizeRoles(Role.Teacher)]
+    public class ReviewerController : ApiControllerBase
+    {
+        [HttpGet(ApiRoutes.Reviewer.GetReviewerByProgramId)]
+        public async Task<ActionResult<ReviewerDto>> GetReviewerByProgramIdAsync(
+            [FromRoute] int educationalProgramId)
+        {
+            return await Mediator.Send(new GetReviewerByProgramIdQuery { EducationalProgramId = educationalProgramId });
+        }
+
+        [HttpPut(ApiRoutes.Reviewer.Base)]
+        public async Task<ActionResult> UpdateReviewerAsync(
+            [FromBody] UpdateReviewerCommand command)
+        {
+            await Mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
